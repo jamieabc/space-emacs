bb7e2a12a3ba767b7c4af808327b6ea8c358e796
Aaron Chang <aaron.chang@quadas.com>
2018-11-24 17:53:56 +0800

    add layer find-file

---
+++ layers/find-file/config.el
@@ -0,0 +1,12 @@
+(defvar ffip-patterns '(
+                        "*.rb" "*.js" "*.yml" "*.css" "*.scss" "*.xml" "*.tmpl" "*.json" "*.md"
+                        "*.lock" "*.sh" "*.java" "*.example" "*.txt" "*.el" "*.hdl" "*.tst" "*.cmp"
+                        "*.erb" "*.php" "*.m" "*.conf" "*.go" "*"
+                        )
+  "Searchable file types."
+  )
+
+(defvar ffip-prune-patterns '(
+                              "*/.git/*" "*/node_modules/*" "*/dist/*"
+                              )
+  )
---
+++ layers/find-file/keybindings.el
@@ -0,0 +1,2 @@
+(define-key evil-normal-state-map (kbd "SPC f p") #'find-file-in-project)
+(global-set-key (kbd "C-c p f") #'find-file-in-project)
