e15bf773edbb1c522516f9570a9558b2695f105d
Aaron Chang <aaron.chang@quadas.com>
2018-11-27 17:51:11 +0800

    add my-cucumber layer

---
+++ layers/my-cucumber/packages.el
@@ -0,0 +1,16 @@
+(defconst my-cucumber-packages
+  '(feature-mode))
+
+(defun my-cucumber/init-feature-mode ()
+  (use-package feature-mode
+    :defer t
+    :mode (("\\.feature\\'" . feature-mode))
+    :init (progn (setq feature-step-search-path "features/**/*steps.rb"))
+    :config
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'feature-mode
+        "s" 'feature-verify-scenario-at-pos
+        "v" 'feature-verify-all-scenarios-in-buffer
+        "f" 'feature-verify-all-scenarios-in-project
+        "g" 'feature-goto-step-definition
+        "." 'feature-goto-step-definition))))
