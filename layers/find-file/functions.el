b1c39c228e362f716c6c77b79e0d909750ccf52a
Aaron Chang <xesuschang@gmail.com>
2018-12-05 11:34:24 +0000

    remove my package find-file

--- layers/find-file/functions.el
+++
@@ -1,5 +0,0 @@
-(defun find-file/setup ()
-  "Find file in project."
-  (interactive)
-  (setq-local ffip-patterns find-file/fit-patterns)
-  (setq-local ffip-prune-patterns find-file/ffip-prune-patterns))
--- layers/find-file/keybindings.el
+++
@@ -1,3 +0,0 @@
-(define-key evil-normal-state-map (kbd "SPC f p") #'find-file-in-project)
-(define-key evil-normal-state-map (kbd "SPC f i") #'ffip-show-diff)
-(global-set-key (kbd "C-c p f") #'find-file-in-project)
